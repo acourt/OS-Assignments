8de62d3f345474e9ab21d7563ae2ff9652f8bf18
Alexandre Courtemanche <alex.courtemanche1@gmail.com>
2012-03-21 01:43:24 -0400

    Added some signal protection to the semaphore P and V

--- A3/MyThreads.c
+++ A3/MyThreads.c
@@ -16,10 +16,14 @@ mythread_control_block *cur_context_ctrl_block;
 int num_threads = 0;
 int quantum_size = 1; // Quantum size in nanoseconds
 
+
+Semaphore* semaphore_table[255];
+int next_semaphore_index;
+
+
 List* threads;
-
+List* run_queue;
 List* semaphores;
-
 
 
 /*
@@ -71,13 +75,13 @@ int init_my_threads()
 {
 	// Create the run queue
 	threads = list_create(NULL);
-	
+	run_queue = list_create(NULL);
 	// Create the semaphores
 	
 	// Set up the prememption process
 	struct itimerval tval;
 	setup_signals();
-	sigset(SIGALRM, scheduler);
+
 }	
 	
 int create_my_thread(char *threadname, void (*threadfunc)(), int stacksize)
@@ -119,38 +123,78 @@ int create_semaphore(int value)
 	Semaphore* sema = malloc(sizeof(Semaphore));
 	sema->value = value;
 	sema->wait_queue = list_create(NULL);
-	return (int)sema;
+	
+	semaphore_table[next_semaphore_index++] = sema;
+	
+	return (next_semaphore_index -1);
 }
 
 void semaphore_wait(int semaphore)
 {
+	if(semaphore_table[semaphore] == NULL)
+	{
+		printf("Error: Tried to wait on a semaphore that was not initialized");
+		exit(-1);
+	}
+	
+	// Retrieve the indexed semaphore
+	Semaphore * sema = semaphore_table[semaphore];
+	
 	// Mask signal interrupts
-	Semaphore* sema = (Semaphore*) semaphore;
+	sigrelse(SIGALRM);
+	
+	// P
 	sema->value--;
 	
 	if(sema->value < 0)
 	{
-		// Wait in the run queue
+		// Remove the current context from the end of the run queue
+		list_pop(run_queue);
+		// Wait in the semaphore's wait queue queue
 		list_append(sema->wait_queue, cur_context_ctrl_block);
 		cur_context_ctrl_block->state = BLOCKED;
 	}
 	
 	// Unmask signal interrupts
+	sighold(SIGALRM);
 	
 	// Wait until the thread becomes unblocked
 	while(cur_context_ctrl_block->state != RUNNABLE)
 	{
-		thread_yield();
+		// Switch to the scheduler program
 	} 
 }
 
-void thread_yield()
-{
-	
-}
 
 void semaphore_signal(int semaphore)
 {
+	if(semaphore_table[semaphore] == NULL)
+	{
+		printf("Error: Tried to wait on a semaphore that was not initialized");
+		exit(-1);
+	}
+	
+	// Retrieve the indexed semaphore
+	Semaphore * sema = semaphore_table[semaphore];
+	
+	// Mask signal interrupts
+	sigrelse(SIGALRM);
+	
+	mythread_control_block* next_in_line = list_shift(sema->wait_queue);
+	
+	// If there are no more threads waiting on this semaphores
+	if(next_in_line == NULL)
+	{
+		sema->value++;
+	}
+	else // Unqueue the next thread and put it in the run queue
+	{
+		next_in_line->state = RUNNABLE;
+		list_append(run_queue, cur_context_ctrl_block);
+	}
+	
+	// UnMask signal interrupts
+	sighold(SIGALRM);
 	
 }
 
@@ -163,10 +207,12 @@ void scheduler()
 {
 	printf("scheduling out thread %d\n", cur_context_ctrl_block->thread_id);
 	
-	// Scheduling algorithm
+	// Take the next context in line and put it in the back of the queue for when it returns
+	cur_context_ctrl_block = list_shift(run_queue);
+	list_append(run_queue, cur_context_ctrl_block);
 	
-	printf("scheduling in thread %d\n", cur_context_ctrl_block->thread_id );
-
+	printf("scheduling in thread %d\n", cur_context_ctrl_block->thread_id);
+	
 	setcontext(&(cur_context_ctrl_block->context)); /* go */
 }
 
